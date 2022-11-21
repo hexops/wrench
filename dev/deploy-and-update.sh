adbf68ddd6ea5388fe3fcea5ad48b29e228698da
Stephen Gutekanst <stephen@hexops.com>
2022-11-20 22:50:43 -0700

    dev: install golang on host
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- dev/deploy-and-update.sh
+++ dev/deploy-and-update.sh
@@ -8,4 +8,9 @@ ssh $remote << EOF
   apt-get install -y git ssh
   apt-get update -y
   apt-get upgrade -y
+
+  curl -OL https://go.dev/dl/go1.19.3.linux-amd64.tar.gz
+  sudo rm -rf /usr/local/go
+  sudo tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
+  sudo ln -s /usr/local/go/bin/go /usr/local/bin/go
 EOF
