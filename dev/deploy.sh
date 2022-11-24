4431f3def57f3949a1117660511c2f648b3d9b70
Stephen Gutekanst <stephen@hexops.com>
2022-11-23 17:14:50 -0700

    dev: wait for scp to succeed
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -7,7 +7,12 @@ source ./dev/remote.sh
 GOARCH="amd64" GOOS="linux" go build -o bin/wrench .
 
 ssh $remote -f 'sudo systemctl stop wrench'
-scp ./bin/wrench $remote:/usr/local/bin/wrench
+
+while ! scp ./bin/wrench $remote:/usr/local/bin/wrench
+do
+    echo 'waiting to scp'
+    sleep 1
+done
 
 scp ./dev/systemd/wrench.service $remote:/etc/systemd/system/wrench.service
 scp ./dev/systemd/wrench-start.sh $remote:/usr/local/bin/wrench-start.sh
