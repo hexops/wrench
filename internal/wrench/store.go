90a5c3b8becf62418ced3fc5a22086cc113ca930
Stephen Gutekanst <stephen@hexops.com>
2022-12-17 16:10:02 -0700

    store: add runner jobs
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- internal/wrench/store.go
+++ internal/wrench/store.go
@@ -3,6 +3,7 @@ package wrench
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
 	"strings"
 	"time"
 
@@ -52,6 +53,16 @@ func (s *Store) ensureSchema() error {
 			created_at TIMESTAMP NOT NULL,
 			expires_at TIMESTAMP,
 			PRIMARY KEY (cache_name, key)
+		);
+		CREATE TABLE IF NOT EXISTS runner_jobs (
+			id TEXT PRIMARY KEY NOT NULL,
+			state TEXT NOT NULL,
+			title TEXT NOT NULL,
+			target_runner_id TEXT NOT NULL,
+			target_runner_arch TEXT NOT NULL,
+			payload TEXT NOT NULL
+			updated_at TIMESTAMP NOT NULL,
+			created_at TIMESTAMP NOT NULL,
 		);
 	`)
 	return err
@@ -140,6 +151,136 @@ func (s *Store) Runners(ctx context.Context) ([]api.Runner, error) {
 		runners = append(runners, runner)
 	}
 	return runners, rows.Err()
+}
+
+type JobState string
+
+const (
+	JobStateStarting JobState = "starting"
+	JobStateRunning  JobState = "running"
+	JobStateFinished JobState = "finished"
+	JobStateErrored  JobState = "errored"
+)
+
+type JobPayload struct {
+	Script []string
+}
+
+type Job struct {
+	ID                               string
+	State                            JobState
+	Title                            string
+	TargetRunnerID, TargetRunnerArch string
+	Payload                          JobPayload
+	Updated, Created                 time.Time
+}
+
+func (s *Store) UpsertRunnerJob(ctx context.Context, job Job) error {
+	now := time.Now()
+	job.Updated = now
+	job.Created = now
+	payload, err := json.Marshal(job.Payload)
+	if err != nil {
+		return errors.Wrap(err, "Marshal")
+	}
+	q := sqlf.Sprintf(
+		`INSERT INTO runner_jobs(
+			id,
+			state,
+			title,
+			target_runner_id,
+			target_runner_arch,
+			payload,
+			updated_at,
+			created_at
+		) VALUES (%v, %v, %v, %v, %v, %v, %v, %v)
+		ON CONFLICT(id) DO UPDATE SET
+			state = %v,
+			title = %v,
+			target_runner_id = %v,
+			target_runner_arch = %v,
+			payload = %v,
+			updated_at = %v
+		WHERE id = %v`,
+		job.ID,
+		job.State,
+		job.Title,
+		job.TargetRunnerID,
+		job.TargetRunnerArch,
+		string(payload),
+		job.Updated,
+		job.Created,
+		job.State,
+		job.Title,
+		job.TargetRunnerID,
+		job.TargetRunnerArch,
+		string(payload),
+		job.Updated,
+	)
+	_, err = s.db.ExecContext(ctx, q.Query(sqlf.SimpleBindVar), q.Args()...)
+	return err
+}
+
+const jobFields = `
+	id,
+	state,
+	title,
+	target_runner_id,
+	target_runner_arch,
+	payload,
+	updated_at,
+	created_at
+`
+
+func (s *Store) JobByID(ctx context.Context, id string) (*Job, error) {
+	q := sqlf.Sprintf(`SELECT `+jobFields+` FROM runner_jobs WHERE id = %v`, id)
+
+	row := s.db.QueryRowContext(ctx, q.Query(sqlf.SimpleBindVar), q.Args()...)
+	job, err := s.scanJob(row.Scan)
+	if err != nil {
+		return nil, errors.Wrap(err, "scanJob")
+	}
+	return job, nil
+}
+
+func (s *Store) JobsByState(ctx context.Context) ([]Job, error) {
+	q := sqlf.Sprintf(`SELECT ` + jobFields + ` FROM runner_jobs ORDER BY jobid`)
+
+	rows, err := s.db.QueryContext(ctx, q.Query(sqlf.SimpleBindVar), q.Args()...)
+	if err != nil {
+		return nil, errors.Wrap(err, "QueryContext")
+	}
+
+	var jobs []Job
+	for rows.Next() {
+		job, err := s.scanJob(rows.Scan)
+		if err != nil {
+			return nil, errors.Wrap(err, "scanJob")
+		}
+		jobs = append(jobs, *job)
+	}
+	return jobs, rows.Err()
+}
+
+func (s *Store) scanJob(scan func(...any) error) (*Job, error) {
+	var j Job
+	var payload string
+	if err := scan(
+		&j.ID,
+		&j.State,
+		&j.Title,
+		&j.TargetRunnerID,
+		&j.TargetRunnerArch,
+		&payload,
+		&j.Updated,
+		&j.Created,
+	); err != nil {
+		return nil, errors.Wrap(err, "Scan")
+	}
+	if err := json.Unmarshal([]byte(payload), &j.Payload); err != nil {
+		return nil, errors.Wrap(err, "Unmarshal")
+	}
+	return &j, nil
 }
 
 func (s *Store) CacheSet(ctx context.Context, cacheName, key, value string, expires *time.Time) error {
