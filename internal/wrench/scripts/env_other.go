d57d60ddb04f0d9456dd869488d8f710dfb83507
Stephen Gutekanst <stephen@hexops.com>
2022-12-23 01:50:06 -0700

    scripts: fix bug in env var setting
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- internal/wrench/scripts/env_other.go
+++ internal/wrench/scripts/env_other.go
@@ -11,13 +11,13 @@ import (
 func setEnvPermanent(key, value string) error {
 	switch runtime.GOOS {
 	case "darwin":
-		err := AppendToFile("/System/Volumes/Data/private/etc/zshrc", "export %s=%s", key, value)()
+		err := AppendToFile("/System/Volumes/Data/private/etc/zshrc", "\nexport %s=%s\n", key, value)()
 		if err != nil {
 			return errors.Wrap(err, "appending to system zshrc")
 		}
 		return nil
 	case "linux":
-		err := AppendToFile("/etc/profile.d/wrench.sh", "%s=%s", key, value)()
+		err := AppendToFile("/etc/profile.d/wrench.sh", "\n%s=%s\n", key, value)()
 		if err != nil {
 			return errors.Wrap(err, "appending to /etc/profile.d/wrench.sh")
 		}
@@ -29,13 +29,13 @@ func setEnvPermanent(key, value string) error {
 func appendEnvPermanent(key, value string) error {
 	switch runtime.GOOS {
 	case "darwin":
-		err := AppendToFile("/System/Volumes/Data/private/etc/zshrc", "export %s=$%s:%s", key, key, value)()
+		err := AppendToFile("/System/Volumes/Data/private/etc/zshrc", "\nexport %s=$%s:%s\n", key, key, value)()
 		if err != nil {
 			return errors.Wrap(err, "appending to system zshrc")
 		}
 		return nil
 	case "linux":
-		err := AppendToFile("/etc/profile.d/wrench.sh", "%s=$%s:%s", key, key, value)()
+		err := AppendToFile("/etc/profile.d/wrench.sh", "\n%s=$%s:%s\n", key, key, value)()
 		if err != nil {
 			return errors.Wrap(err, "appending to /etc/profile.d/wrench.sh")
 		}
