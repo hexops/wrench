501558342d593452b0866693d0569ed30e598e3c
Stephen Gutekanst <stephen@hexops.com>
2022-12-22 18:13:49 -0700

    add "wrench script install-go"
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

---
+++ internal/wrench/scripts/install_go.go
@@ -0,0 +1,73 @@
+package scripts
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/hexops/wrench/internal/errors"
+)
+
+func init() {
+	Scripts = append(Scripts, Script{
+		Command:     "install-go",
+		Args:        []string{"force"},
+		Description: "ensure that wrench's desired Go version is installed",
+		Execute: func(args ...string) error {
+			wantGoVersion := "1.19.4"
+
+			force := len(args) == 1 && args[0] == "true"
+			goVersion, err := Output("go version")
+			if err == nil && strings.Contains(goVersion, wantGoVersion) && !force {
+				return nil
+			}
+
+			pathToGo, err := exec.LookPath("go")
+			if err == nil {
+				pathToGo, _ = filepath.Abs(pathToGo)
+			}
+
+			// Download the Go archive
+			extension := "tar.gz"
+			if runtime.GOOS == "windows" {
+				extension = "zip"
+			}
+			url := fmt.Sprintf("https://go.dev/dl/go%s.%s-%s.%s", wantGoVersion, runtime.GOOS, runtime.GOARCH, extension)
+			archiveFilePath := "golang." + extension
+			_ = os.RemoveAll(archiveFilePath)
+			defer os.RemoveAll(archiveFilePath)
+			err = DownloadFile(url, archiveFilePath)()
+			if err != nil {
+				return errors.Wrap(err, "DownloadFile")
+			}
+
+			// Remove existing install dir if it exists.
+			_ = os.RemoveAll("golang")
+
+			goBinaryLocation, err := filepath.Abs("go/bin/go")
+			if err != nil {
+				return errors.Wrap(err, "Abs")
+			}
+			if pathToGo != goBinaryLocation {
+				fmt.Fprintln(os.Stderr, "warning: existing Go installation may conflict:", pathToGo)
+			}
+			fmt.Fprintln(os.Stderr, "installing to:", goBinaryLocation)
+
+			// Update system-wide env vars.
+			err = EnsureOnPathPermanent(filepath.Dir(goBinaryLocation))
+			if err != nil {
+				return errors.Wrap(err, "EnsureOnPathPermanent")
+			}
+
+			// Extract the Go archive
+			err = ExtractArchive(archiveFilePath, ".")()
+			if err != nil {
+				return errors.Wrap(err, "ExtractArchive")
+			}
+			return nil
+		},
+	})
+}
